73108242454e210b9ba3497059154807eb12907e
unknown <cparikh7@gmail.com>
2013-08-20 18:00:25 +0530

    Fixed bugs

--- monochrome.py
+++ monochrome.py
@@ -37,24 +37,19 @@ def to_ascee(path,destination):
     image_file = Image.open(path) # open colour image
     image_file = image_file.convert('L')
 #   image_file = image_file.resize((384,216))
-    new_image = """<html>
-                   <body>
-                   <pre  style="font-size:1px">\n
-		"""
+    new_image = "<html><body><pre style=\"font-size:1px\">\n"
+    print new_image
     for y in range(image_file.size[1]):
 	for x in range(image_file.size[0]):
 	    pixel = 255 - image_file.getpixel((x,y))
             new_image += asceescale[lvl.level(pixel)] 
         new_image += "\n"
-    new_image += """
-		</pre>
-		</body>
-		</html>"""
+    new_image += "</pre></body></html>"
     text_file = open(destination, "w")
     text_file.write(new_image)
     text_file.close()
 
-if(len(sys.argv)>2):
+if(len(sys.argv)>3):
     getReplacements(sys.argv[3])
 
 to_ascee(sys.argv[1],sys.argv[2])  
